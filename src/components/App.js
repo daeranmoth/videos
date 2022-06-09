bf14b0e75dbaa1ed801010028638857904118682
gary camboulives <gary.camboulives@gmail.com>
2022-06-09 17:00:54 +0200

    final version with a custom hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
+//import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
+import useVideos from '../hooks/useVideos';
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
+
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos('buidlings');
 
   useEffect(() => {
-    onTermSubmit('buildings');
-  }, []);
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
-  const onTermSubmit = async (term) => {
-    const response = await youtube.get('/search', {
-      params: {
-        q: term
-      }
-    });
-
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
-
-  const onVideoSelect = (video) => {
-    setSelectedVideo(video);
-  }
-
+// if you give me a default search term, i will give you a way to search for videos and a list of videos
   return (
     <div className='ui container'>
-      <SearchBar onFormSubmit={onTermSubmit}/>
+      <SearchBar onFormSubmit={search}/>
       <div className='ui grid'>
         <div className='ui row'>
           <div className='eleven wide column'>
           <VideoDetail video={selectedVideo} />
           </div>
           <div className='five wide column'>
-          <VideoList onVideoSelect={onVideoSelect} videos={videos}/>
+          <VideoList onVideoSelect={setSelectedVideo} videos={videos}/>
           </div>
         </div>
       </div>
