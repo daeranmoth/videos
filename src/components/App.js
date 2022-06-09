bedb2253424167893b7c8f50d2d13e0094aa3de4
gary camboulives <gary.camboulives@gmail.com>
2022-06-09 15:56:15 +0200

    App refacto with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  componentDidMount() {
-    this.onTermSubmit('dresses');
-  }
+  useEffect(() => {
+    onTermSubmit('buildings');
+  }, []);
 
-  onTermSubmit = async (term) => {
+  const onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
 
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0]
-     });
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   };
 
-  onVideoSelect = (video) => {
-    this.setState({selectedVideo: video});
+  const onVideoSelect = (video) => {
+    setSelectedVideo(video);
   }
 
-  render() {
-    return (
+  return (
     <div className='ui container'>
-      <SearchBar onFormSubmit={this.onTermSubmit}/>
+      <SearchBar onFormSubmit={onTermSubmit}/>
       <div className='ui grid'>
         <div className='ui row'>
           <div className='eleven wide column'>
-          <VideoDetail video={this.state.selectedVideo} />
+          <VideoDetail video={selectedVideo} />
           </div>
           <div className='five wide column'>
-          <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+          <VideoList onVideoSelect={onVideoSelect} videos={videos}/>
           </div>
         </div>
       </div>
     </div>
     );
-  }
-}
+
+};
 
 export default App;
